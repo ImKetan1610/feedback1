7e05ae2ee267e297287ebf625ee35dea52dc6ecc
ImKetan1610 <ketan.makode@gmail.com>
2022-11-22 18:46:58 +0530

    added framer motion for feedback items

--- feedback_app/src/components/FeedbackList.jsx
+++ feedback_app/src/components/FeedbackList.jsx
@@ -1,4 +1,5 @@
 
+import { motion, AnimatePresence } from "framer-motion"
 import PropTypes from "prop-types"
 import FeedbackItem from "./FeedbackItem"
 
@@ -10,11 +11,23 @@ function FeedbackList ( { feedback,handleDelete }) {
         )
     }
 
-    return <div className="feedback-List">
-            {feedback.map((item) => (
-                <FeedbackItem key={item.id} item={item} handleDelete = { handleDelete } />
-            ))}
-        </div>    
+    return (
+        <div className="feedback-List">
+            <AnimatePresence>
+                {feedback.map((item) => (
+                    <motion.div key = {item.id} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} >
+                        <FeedbackItem key={ item.id } item={ item } handleDelete = { handleDelete } />
+                    </motion.div>
+                ))}
+            </AnimatePresence>
+        </div>  
+    )
+
+    // return <div className="feedback-List">
+    //         {feedback.map((item) => (
+    //             <FeedbackItem key={item.id} item={item} handleDelete = { handleDelete } />
+    //         ))}
+    //     </div>    
 }
 
 FeedbackList.propTypes = {
