e1f9b2eec19f875a572bd7bb72db643e23d24bdc
ImKetan1610 <ketan.makode@gmail.com>
2022-11-23 11:56:45 +0530

    adding changes according to the react router dom version 6

--- feedback_app/src/App.js
+++ feedback_app/src/App.js
@@ -1,6 +1,6 @@
 
 import { useState } from "react";
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { v4 as uuidv4 } from 'uuid';
 import Header from "./components/Header";
 // import FeedbackItem from "./components/FeedbackItem";
@@ -31,12 +31,16 @@ function App() {
       <Header />
       {/* <FeedbackItem /> */}
       <div className='container'>
-        <Route exact path="/">
-          <FeedbackForm handleAdd = { addFeedback } />
-          <FeedbackStats feedback = {feedback} />
-          <FeedbackList feedback={feedback}  handleDelete={deleteFeedback} />
-        </Route>
-        <Route path='/about' component={ AboutPage } />
+        <Routes>
+          <Route exact path="/" element = {
+            <>
+              <FeedbackForm handleAdd = { addFeedback } />
+              <FeedbackStats feedback = {feedback} />
+              <FeedbackList feedback={feedback}  handleDelete={deleteFeedback} />
+            </>
+          }></Route>
+          <Route path='/about' element={ <AboutPage /> } />
+        </Routes>
       </div>
     </Router>
   );
