15084690c30086cb03962f3d77ae55bed472b68c
ImKetan1610 <ketan.makode@gmail.com>
2022-11-28 13:30:17 +0530

    Added delete feedback feature to json server

--- feedback_app/src/context/FeedbackContext.jsx
+++ feedback_app/src/context/FeedbackContext.jsx
@@ -25,8 +25,12 @@ export const FeedbackProvider = ({children}) => {
     }
 
     //Delete feedback
-    const deleteFeedback = ( id ) => {
+    const deleteFeedback = async ( id ) => {
         if(window.confirm("Are you sure you want to delete?")){
+            await fetch (`/feedback/${id}`,{
+                method: "DELETE"
+            })
+
           setFeedback(feedback.filter((item)=>item.id !== id))
         }
     }
